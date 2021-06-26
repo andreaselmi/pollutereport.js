1d0c586bb29af3b977302f553cc1dd1686a08340
andreaselmi <andreaselmi90@gmail.com>
2021-06-26 14:57:18 +0200

    create post / delete post

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,21 @@
+const mongoose = require("mongoose");
 const router = require("express").Router();
 const auth = require("../middlewares/auth");
 
 const { Post, postValidator } = require("../models/Post");
 const { User } = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.status(200).send("Post page");
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find().populate("author");
+    res.send(posts);
+  } catch (error) {
+    //TODO error handler
+    res.status(500).send("Something goes wrong");
+  }
 });
 
+//create new post
 router.post("/", auth, async (req, res) => {
   const { error } = postValidator.validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
@@ -35,5 +43,29 @@ router.post("/", auth, async (req, res) => {
     res.status(500).send("Something goes wrong");
   }
 });
+
+//Delete post
+router.delete("/:id", auth, async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id))
+    return res.status(400).send("Your id is not valid");
+
+  const post = await Post.findById(req.params.id);
+  if (!post) return res.status(404).send("Post not found");
+
+  //TODO migliorare
+
+  if (post.author !== req.user._id && !req.user.isAdmin)
+    return res.status(401).send("You can delete only your post");
+
+  try {
+    await post.remove();
+    res.send(post);
+  } catch (error) {
+    //TODO error handler
+    res.status(500).send("Something goes wrong");
+  }
+});
+
+//Update post
 
 module.exports = router;
