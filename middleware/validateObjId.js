bf540da80e2f4671ca844bca8fc3760dcf656b27
andreaselmi <andreaselmi90@gmail.com>
2021-07-04 19:16:55 +0200

    fixed

--- middleware/validateObjId.js
+++ middleware/validateObjId.js
@@ -4,9 +4,5 @@ module.exports = (req, res, next) => {
   if (req.params.id && !mongoose.Types.ObjectId.isValid(req.params.id))
     return res.status(404).send("Invalid ID");
 
-  //TODO valutare se inserire la validazione del body
-  // if (req.body.id && !mongoose.Types.ObjectId.isValid(req.body.id))
-  //   return res.status(404).send("Invalid ID");
-
   next();
 };
